1f85588bab05def05c759fd5eabc78ddb1d4068a
pdiakumis <pdiakumis@unimelb.edu.au>
2019-11-07 22:41:42 +1100

    modularise illumina funcs

--- app.js
+++ app.js
@@ -1,40 +1,19 @@
 const express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    request = require("request"),
-    path = require("path"),
-    yaml = require("js-yaml"),
-    fs = require("fs"),
-    os = require("os");
+      app = express(),
+      bodyParser = require("body-parser"),
+      request = require("request"),
+      path = require("path"),
+      os = require("os"),
+      illumina = require("./illumina");
 
 const port = 3000;
-const illumina_url = "https://aps2.platform.illumina.com/v1/"
-
-const usernames = {
-    "567d89e4-de8b-3688-a733-d2a979eb510e": "PD",
-    "7eec7332-f780-3edc-bb70-c4f711398f1c": "RV",
-    "8abf754b-e94f-3841-b44b-75d10d33588b": "SK",
-    "c9688651-7872-3753-8146-ffa41c177aa1": "VS",
-};
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 // access /public files from /static
 app.use("/static", express.static(path.join(__dirname, "public")));
 
-
-function read_iap_token(t) {
-    let token;
-    try {
-        token = yaml.safeLoad(fs.readFileSync(t, 'utf8'));
-        token = token['access-token'];
-    } catch (e) {
-        console.log(e);
-    }
-    return token;
-}
-
-const token = read_iap_token(path.join(os.homedir(), '.iap/.session.yaml'));
+const token = illumina.read_iap_token(path.join(os.homedir(), '.iap/.session.yaml'));
 
 app.get("/", (req, res) => res.render("home"));
 
@@ -48,12 +27,12 @@ let options = {
 
 app.get("/tasks", (req, res) => {
     let opts = options;
-    opts.url = illumina_url + 'tasks?pageSize=1000';
+    opts.url = illumina.base_url + 'tasks?pageSize=1000';
 
     request(opts, (error, response, body) => {
         if (!error && response.statusCode == 200) {
             const tasks = JSON.parse(body);
-            res.render("tasks", { tasks: tasks, usernames: usernames });
+            res.render("tasks", { tasks: tasks, id2username: illumina.id2username });
         }
     });
 });
@@ -61,13 +40,13 @@ app.get("/tasks", (req, res) => {
 app.get("/tasks/:taskid", (req, res) => {
     let opts = options;
     const taskid = req.params.taskid;
-    opts.url = illumina_url + `tasks/${taskid}`;
+    opts.url = illumina.base_url + `tasks/${taskid}`;
 
     request(opts, (error, response, body) => {
         if (!error && response.statusCode == 200) {
             const task_info = JSON.parse(body);
             console.log(task_info);
-            res.render("taskid", { task_info: task_info, taskid: taskid, usernames: usernames });
+            res.render("taskid", { task_info: task_info, taskid: taskid, id2username: illumina.id2username });
         }
     });
 });
@@ -78,7 +57,7 @@ app.get("/tasks/runs/:runid", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-    res.send("You're a STAR!");
+    res.send("Oops. Wrong URL!");
 });
 
 app.listen(port, () => {
---
+++ illumina.js
@@ -0,0 +1,33 @@
+const yaml = require("js-yaml"),
+      fs = require("fs");
+
+const base_url = "https://aps2.platform.illumina.com/v1/";
+
+id2username = function (id) {
+    let username = {
+    "567d89e4-de8b-3688-a733-d2a979eb510e": "PD",
+    "7eec7332-f780-3edc-bb70-c4f711398f1c": "RV",
+    "8abf754b-e94f-3841-b44b-75d10d33588b": "SK",
+    "c9688651-7872-3753-8146-ffa41c177aa1": "VS",
+    }
+
+    return username[id];
+};
+
+read_iap_token = function (t) {
+    let token;
+    try {
+        token = yaml.safeLoad(fs.readFileSync(t, 'utf8'));
+        token = token['access-token'];
+    } catch (e) {
+        console.log(e);
+    }
+    return token;
+}
+
+module.exports = {
+    read_iap_token: read_iap_token,
+    base_url: base_url,
+    id2username: id2username,
+}
+
