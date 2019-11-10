6e9e124d2bd84a46594402ca4dd6f842a7284ca7
pdiakumis <pdiakumis@unimelb.edu.au>
2019-11-10 22:18:23 +1100

    cleanup

--- app.js
+++ app.js
@@ -1,188 +1,223 @@
 const express = require("express"),
-      app = express(),
-      bodyParser = require("body-parser"),
-      request = require("request"),
-      path = require("path"),
-      os = require("os"),
-      utils = require("./utils"),
-      illumina = require("./illumina");
+  request = require("request"),
+  path = require("path"),
+  os = require("os"),
+  utils = require("./utils"),
+  illumina = require("./illumina");
 
 const port = process.env.PORT || 3000;
 const pageSize = 100;
 
+const app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
 // access /public files from /static
 app.use("/static", express.static(path.join(__dirname, "public")));
 
-const token = illumina.read_iap_token(path.join(os.homedir(), '.iap/.session.yaml'));
+const token = illumina.read_iap_token(
+  path.join(os.homedir(), ".iap/.session.yaml")
+);
+
+let options = {
+  headers: {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json"
+  },
+  baseUrl: illumina.base_url,
+  json: true
+};
 
 app.get("/", (req, res) => res.render("home"));
 
-let options = {
-    headers: {
-        Authorization: 'Bearer ' + token,
-        'Content-Type': 'application/json'
-    },
-    baseUrl : illumina.base_url,
-    json: true
-};
+//------------------------ Task Routes -------------------------------//
 
-//------------------------ TES Routes -------------------------------//
 app.get("/tasks", (req, res) => {
-    let opts = options;
-    opts.url = `/tasks?pageSize=${pageSize}`;
+  let opts = options;
+  opts.url = "/tasks";
+  opts.qs = {
+    pageSize: pageSize,
+    sort: "timeCreated desc"
+  };
 
-    request.get(opts, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            res.render("tes/tasks", {
-                tasks: body,
-                id2username: illumina.id2username,
-                format_date: utils.format_date
-            });
-        } else {
-            console.log("There was an error: " + error);
-        }
-    });
+  request.get(opts, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      res.render("tes/tasks", {
+        tasks: body,
+        id2username: illumina.id2username,
+        format_date: utils.format_date
+      });
+    } else {
+      utils.print_error(error);
+    }
+  });
 });
 
 app.get("/tasks/runs", (req, res) => {
-    let opts = options;
-    opts.url = `/tasks/runs?pageSize=${pageSize}`;
+  let opts = options;
+  opts.url = "/tasks/runs";
+  opts.qs = {
+    pageSize: pageSize,
+    sort: "timeCreated desc"
+  };
 
-    request.get(opts, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            res.render("tes/taskruns", {
-                truns: body,
-                id2username: illumina.id2username,
-                format_date: utils.format_date
-            });
-        } else {
-            console.log("There was an error: " + error);
-        }
-    });
+  request.get(opts, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      res.render("tes/taskruns", {
+        truns: body,
+        id2username: illumina.id2username,
+        format_date: utils.format_date
+      });
+    } else {
+      utils.print_error(error);
+    }
+  });
 });
 
-
 app.get("/tasks/:taskid", (req, res) => {
-    let opts = options;
-    const taskid = req.params.taskid;
-    opts.url = `/tasks/${taskid}`;
+  let opts = options;
+  const taskid = req.params.taskid;
+  opts.url = `/tasks/${taskid}`;
 
-    request.get(opts, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            res.render("tes/taskid", {
-                task_info: body,
-                taskid: taskid,
-                id2username: illumina.id2username,
-                format_date: utils.format_date
-            });
-        } else {
-            console.log("There was an error: " + error);
-        }
-    });
+  request.get(opts, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      res.render("tes/taskid", {
+        task_info: body,
+        taskid: taskid,
+        id2username: illumina.id2username,
+        format_date: utils.format_date
+      });
+    } else {
+      utils.print_error(error);
+    }
+  });
 });
 
 app.get("/tasks/:taskid/versions", (req, res) => {
-    let opts = options;
-    const taskid = req.params.taskid;
-    opts.url = `/tasks/${taskid}/versions`;
+  let opts = options;
+  const taskid = req.params.taskid;
+  opts.url = `/tasks/${taskid}/versions`;
 
-    request.get(opts, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            res.send(body);
-            // res.render("tes/taskid", {
-            //     task_info: task_info,
-            //     taskid: taskid,
-            //     id2username: illumina.id2username,
-            //     format_date: utils.format_date
-            // });
-        } else {
-            console.log("There was an error: " + error);
-        }
-    });
+  request.get(opts, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+      // res.render("tes/taskid", {
+      //     task_info: task_info,
+      //     taskid: taskid,
+      //     id2username: illumina.id2username,
+      //     format_date: utils.format_date
+      // });
+    } else {
+      utils.print_error(error);
+    }
+  });
 });
 
 app.get("/tasks/runs/:runid", (req, res) => {
-    let opts = options;
-    const runid = req.params.runid;
-    opts.url = `/tasks/runs/${runid}`;
+  let opts = options;
+  const runid = req.params.runid;
+  opts.url = `/tasks/runs/${runid}`;
 
-    request.get(opts, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            res.render("tes/taskrunid", {
-                trun_info: body,
-                runid: runid,
-                id2username: illumina.id2username,
-                format_date: utils.format_date
-            });
-        } else {
-            console.log("There was an error: " + error);
-        }
-    });
+  request.get(opts, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      res.render("tes/taskrunid", {
+        trun_info: body,
+        runid: runid,
+        id2username: illumina.id2username,
+        format_date: utils.format_date
+      });
+    } else {
+      utils.print_error(error);
+    }
+  });
 });
 
-//------------------------ WES Routes -------------------------------//
-app.get("/workflows", (req, res) => {
-    let opts = options;
-    opts.url = `/workflows?pageSize=${pageSize}`;
+//------------------------ Workflow Routes -------------------------------//
 
-    request.get(opts, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            res.render("wes/workflows", {
-                wflows: body,
-                id2username: illumina.id2username,
-                format_date: utils.format_date
-            });
-        } else {
-            console.log("There was an error: " + error);
-        }
-    });
+app.get("/workflows", (req, res) => {
+  let opts = options;
+  opts.url = "/workflows";
+  opts.qs = {
+    pageSize: pageSize,
+    sort: "timeCreated desc"
+  };
+
+  request.get(opts, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      res.render("wes/workflows", {
+        wflows: body,
+        id2username: illumina.id2username,
+        format_date: utils.format_date
+      });
+    } else {
+      utils.print_error(error);
+    }
+  });
 });
 
 app.get("/workflows/runs", (req, res) => {
-    let opts = options;
-    opts.url = `/workflows/runs?pageSize=${pageSize}`;
+  let opts = options;
+  opts.url = "/workflows/runs";
+  opts.qs = {
+    pageSize: pageSize,
+    sort: "timeCreated desc"
+  };
 
-    request.get(opts, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            res.send(body);
-            // res.render("wes/workflowruns", {
-            //     wruns: wruns,
-            //     id2username: illumina.id2username,
-            //     format_date: utils.format_date
-            // });
-        } else {
-            console.log("There was an error: " + error);
-        }
-    });
+  request.get(opts, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      // res.send(body);
+      res.render("wes/workflowruns", {
+        wruns: body,
+        id2username: illumina.id2username,
+        format_date: utils.format_date
+      });
+    } else {
+      utils.print_error(error);
+    }
+  });
 });
-
 
 app.get("/workflows/:workflowid", (req, res) => {
-    let opts = options;
-    const wflowid = req.params.workflowid;
-    opts.url = `/workflows/${wflowid}`;
+  let opts = options;
+  const wflowid = req.params.workflowid;
+  opts.url = `/workflows/${wflowid}`;
 
-    request.get(opts, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            res.render("wes/workflowid", {
-                wflow_info: body,
-                wflowid: wflowid,
-                id2username: illumina.id2username,
-                format_date: utils.format_date
-            });
-        } else {
-            console.log("There was an error: " + error);
-        }
-    });
+  request.get(opts, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      res.render("wes/workflowid", {
+        wflow_info: body,
+        wflowid: wflowid,
+        id2username: illumina.id2username,
+        format_date: utils.format_date
+      });
+    } else {
+      utils.print_error(error);
+    }
+  });
 });
 
+app.get("/workflows/runs/:runid", (req, res) => {
+  let opts = options;
+  const runid = req.params.runid;
+  opts.url = `/workflows/runs/${runid}`;
+
+  request.get(opts, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+      // res.render("wes/workflowrunid", {
+      //     wrun_info: body,
+      //     runid: runid,
+      //     id2username: illumina.id2username,
+      //     format_date: utils.format_date
+      // });
+    } else {
+      utils.print_error(error);
+    }
+  });
+});
 
 app.get("*", (req, res) => {
-    res.send("Oops. Wrong URL!");
+  res.send("Oops. Wrong URL!");
 });
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
--- utils.js
+++ utils.js
@@ -4,6 +4,11 @@ format_date = function(date) {
     return d.toISOString().split('.')[0]+"Z";
 }
 
+print_error = function(e) {
+    console.log(`There was an error ${e}`);
+}
+
 module.exports = {
-    format_date: format_date
+    format_date: format_date,
+    print_error: print_error
 }
