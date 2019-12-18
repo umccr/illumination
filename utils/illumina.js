00a2a5e4d55aa7ab4d3fa9c599e9194ef593ba30
pdiakumis <pdiakumis@unimelb.edu.au>
2019-12-18 15:13:51 +1100

    add service user account

--- utils/illumina.js
+++ utils/illumina.js
@@ -9,7 +9,8 @@ const id2username = function(id) {
     "7eec7332-f780-3edc-bb70-c4f711398f1c": "FR",
     "1678890e-b107-3974-a47d-0bb532a64ad6": "RV",
     "8abf754b-e94f-3841-b44b-75d10d33588b": "SK",
-    "c9688651-7872-3753-8146-ffa41c177aa1": "VS"
+    "c9688651-7872-3753-8146-ffa41c177aa1": "VS",
+    "57a99faa-ae79-33f8-9736-454a36b06a43": "SU"
   };
 
   return username[id];
