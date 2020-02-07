a98143e2e2924e3ed50b9efbdebf917b5841e00e
pdiakumis <pdiakumis@unimelb.edu.au>
2020-02-07 16:43:09 +1100

    update user IDs

--- utils/illumina.js
+++ utils/illumina.js
@@ -8,14 +8,15 @@ const id2username = function(id) {
     "567d89e4-de8b-3688-a733-d2a979eb510e": "PD",
     "7eec7332-f780-3edc-bb70-c4f711398f1c": "FR",
     "1678890e-b107-3974-a47d-0bb532a64ad6": "RV",
-    "8abf754b-e94f-3841-b44b-75d10d33588b": "SK",
+    "8abf754b-e94f-3841-b44b-75d10d33588b": "SK_old",
+    "d24913a8-676f-39f3-9250-7cf22fbc48c8": "SK",
     "c9688651-7872-3753-8146-ffa41c177aa1": "VS",
     "57a99faa-ae79-33f8-9736-454a36b06a43": "SU",
     "6696900a-96ea-372a-bc00-ca6bbe19bf7b": "Kym",
     "ef928f99-662d-3e9f-8476-303131e9a58a": "Karey",
     "3ed6bc8a-ba5a-3ec3-9e25-361703c7ba20": "Egan-Illumina",
     "b2f0ff65-c77b-37bc-af87-68a89c2f8d27": "AL",
-    "46258763-7c48-3a1c-8c5f-04003bf74e5a": "AL2"
+    "46258763-7c48-3a1c-8c5f-04003bf74e5a": "AL_old"
   };
 
   return username[id];
