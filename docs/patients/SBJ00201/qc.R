e5a0df8d4919379d373113156f0ba7f7dff4dce2
pdiakumis <pdiakumis@unimelb.edu.au>
2020-07-25 22:05:30 +1000

    cleanup

--- docs/patients/SBJ00201/qc.R
+++ docs/patients/SBJ00201/qc.R
@@ -1,20 +1,30 @@
 require(tidyverse)
 require(here)
 
-fn <- list.files(here("nogit/SBJ00201"), full.names = T, recursive = T)
-dn <- dirname(fn) %>% basename()
+fn <- list.files(c(here("nogit/benchmark_time/streamDefaultS3/"),
+                   here("nogit/benchmark_time/streamDirectS3/")),
+                 full.names = T, recursive = T, pattern = "time_metrics.csv")
 
-read_metrics <- function(x, d) {
+read_metrics <- function(x) {
+  sample <- sub("(.*).time_metrics.csv", "\\1", basename(x))
+  mode <- dirname(x) %>% basename()
   readr::read_csv(x, col_names = c("x", "y", "name", "time_hrs", "time_sec"),
                   col_types = cols(.default = "c", time_sec = "d")) %>%
     dplyr::select(name, time_sec, time_hrs) %>%
     dplyr::mutate(name = sub("Time ", "", name),
                   time_sec = floor(time_sec),
                   time_hrs = sub("\\..*", "", time_hrs),
-                  run = d)
+                  sample = sample,
+                  mode = mode)
 }
 
-purrr::map2(fn, dn, read_metrics) %>%
+purrr::map(fn, read_metrics) %>%
   dplyr::bind_rows() %>%
-  tidyr::pivot_wider(id_cols = name, names_from = run, values_from = time_hrs)
+  dplyr::filter(mode == "streamDefaultS3") %>%
+  tidyr::pivot_wider(id_cols = name, names_from = c(sample), values_from = time_hrs)
+
+purrr::map(fn, read_metrics) %>%
+  dplyr::bind_rows() %>%
+  dplyr::filter(mode != "streamDefaultS3") %>%
+  tidyr::pivot_wider(id_cols = name, names_from = c(sample), values_from = time_hrs)
 
--- scripts/download_from_gds/00_grab_gds_paths.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-folder_path="diakumis-dragen/test_tes_TN_align/"
-
-(echo "["; \
-    iap files list gds://${folder_path} -o json --page-size 100 | \
-    jq -j '. | { path: .path, volume: .volumeName }' | \
-    sed "s/}{/},{/g"; echo "]") | \
-    jq -r '. | map([.volume, .path] | join(", ")) | join("\n")'
