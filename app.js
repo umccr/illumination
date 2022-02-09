5a4ea1732dfe9315306db3306aab0cab06c6e676
pdiakumis <peterdiakumis@gmail.com>
2022-02-09 22:56:21 +1100

    update app.js

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const express = require("express"),
   path = require("path");
 
 const indexRouter = require("./routes/index"),
-  tasksRouter = require("./routes/tasks"),
-  workflowsRouter = require("./routes/workflows"),
-  usagesRouter = require("./routes/usages"),
-  healthRouter = require("./routes/health"),
-  regionsRouter = require("./routes/regions"),
-  filesRouter = require("./routes/files"),
-  foldersRouter = require("./routes/folders"),
-  volumesRouter = require("./routes/volumes"),
-  subscriptionsRouter = require("./routes/subscriptions"),
-  tokensRouter = require("./routes/tokens"),
+  projectsRouter = require("./routes/projects"),
+  analysisStoragesRouter = require("./routes/analysisstorages"),
+  usersRouter = require("./routes/users"),
+  bundlesRouter = require("./routes/bundles"),
   workgroupsRouter = require("./routes/workgroups"),
-  projectsRouter = require("./routes/projects");
-
+  regionsRouter = require("./routes/regions"),
+  samplesRouter = require("./routes/samples"),
+  pipelinesRouter = require("./routes/pipelines"),
+  metadatamodelsRouter = require("./routes/metadatamodels"),
+  connectorsRouter = require("./routes/connectors");
 // const listEndpoints = require("express-list-endpoints");
 
 const port = process.env.PORT || 3000;
@@ -24,18 +21,16 @@ app.set("view engine", "ejs");
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/tasks", tasksRouter);
-app.use("/workflows", workflowsRouter);
-app.use("/usages", usagesRouter);
-app.use("/health", healthRouter);
-app.use("/regions", regionsRouter);
-app.use("/files", filesRouter);
-app.use("/folders", foldersRouter);
-app.use("/volumes", volumesRouter);
-app.use("/subscriptions", subscriptionsRouter);
-app.use("/tokens/details", tokensRouter);
-app.use("/workgroups", workgroupsRouter);
 app.use("/projects", projectsRouter);
+app.use("/analysisStorages", analysisStoragesRouter);
+app.use("/users", usersRouter);
+app.use("/bundles", bundlesRouter);
+app.use("/workgroups", workgroupsRouter);
+app.use("/regions", regionsRouter);
+app.use("/samples", samplesRouter);
+app.use("/pipelines", pipelinesRouter);
+app.use("/metadatamodels", metadatamodelsRouter);
+app.use("/connectors", connectorsRouter);
 
 app.get("*", (req, res) => {
   res.status(404).render("404");
