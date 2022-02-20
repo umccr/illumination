e053f7d4c6debe16545f027185e7aeeecc4e1070
pdiakumis <peterdiakumis@gmail.com>
2022-02-20 13:14:54 +1100

    typescript initial support

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ const indexRouter = require("./routes/index"),
 
 const port = process.env.PORT || 3000;
 
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 app.use("/static", express.static(path.join(__dirname, "public")));
 
--- src/utils/id2username.ts
+++ src/utils/id2username.ts
@@ -1,6 +1,9 @@
-const id2username_json: {[key: string]: string} = require("./id2username.json");
+import fs from "fs";
+// import * as id2username_json from "./id2username.json";
+const id2username_json = JSON.parse(fs.readFileSync("./id2username.json", "utf-8"))
+
 const id2username = function (id: string) {
-  return id2username_json[id];
+  id2username_json[id];
 };
 
 module.exports = {
