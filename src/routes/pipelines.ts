ba768bd2e95a7d41ee384f5d849c74037d2f56fd
pdiakumis <peterdiakumis@gmail.com>
2022-03-04 14:55:02 +1100

    add pipelineid referencesets + inputparameters views

--- src/routes/pipelines.ts
+++ src/routes/pipelines.ts
@@ -42,4 +42,42 @@ router.get("/:pipelineid", (req, res) => {
     .catch((error: Error) => utils.print_error(error));
 });
 
+router.get("/:pipelineid/inputParameters", (req, res) => {
+  let opts = request_opts;
+  let qs = req.query;
+  const pipelineid = req.params.pipelineid;
+  opts.url = `/pipelines/${pipelineid}/inputParameters`;
+  opts.params = qs;
+  axios(opts)
+    .then((response: any) => {
+      // res.send(response.data);
+      res.render("pipelines/pipelineinputparameters", {
+       data: response.data,
+       id2username: id2username.id2username,
+       pipelineid: pipelineid,
+       jsonSyntaxHighlight: utils.jsonSyntaxHighlight,
+       });
+    })
+    .catch((error: Error) => utils.print_error(error));
+});
+
+router.get("/:pipelineid/referenceSets", (req, res) => {
+  let opts = request_opts;
+  let qs = req.query;
+  const pipelineid = req.params.pipelineid;
+  opts.url = `/pipelines/${pipelineid}/referenceSets`;
+  opts.params = qs;
+  axios(opts)
+    .then((response: any) => {
+      // res.send(response.data);
+      res.render("pipelines/pipelinereferencesets", {
+       data: response.data,
+       id2username: id2username.id2username,
+       pipelineid: pipelineid,
+       jsonSyntaxHighlight: utils.jsonSyntaxHighlight,
+       });
+    })
+    .catch((error: Error) => utils.print_error(error));
+});
+
 module.exports = router;
