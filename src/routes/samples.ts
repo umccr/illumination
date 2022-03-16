1f9c14de5ccc99fa70aad042bd69a3e6037cbf21
pdiakumis <peterdiakumis@gmail.com>
2022-03-16 20:56:04 +1100

    update samples endpoint

--- src/routes/samples.ts
+++ src/routes/samples.ts
@@ -4,6 +4,7 @@ const axios = require("axios").default;
 const illumina = require("../utils/illumina");
 const utils = require("../utils/utils");
 const request_opts = illumina.request_opts();
+const id2username = require("../utils/id2username");
 
 router.get("/", (req, res) => {
   let opts = request_opts;
@@ -12,11 +13,12 @@ router.get("/", (req, res) => {
   opts.params = qs;
   axios(opts)
     .then((response: any) => {
-      res.send(response.data);
-      //res.render("samples", {
-      // data: response.data,
-      // jsonSyntaxHighlight: utils.jsonSyntaxHighlight,
-      // });
+      // res.send(response.data);
+      res.render("samples", {
+       data: response.data,
+       id2username: id2username.id2username,
+       jsonSyntaxHighlight: utils.jsonSyntaxHighlight,
+       });
     })
     .catch((error: Error) => utils.print_error(error));
 });
