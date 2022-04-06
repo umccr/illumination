42fcb6a826fec7c8d604e41adb3148a9564759bf
pdiakumis <peterdiakumis@gmail.com>
2022-04-06 23:24:17 +1000

    add notificationChannelsId view

--- src/routes/notificationchannels.ts
+++ src/routes/notificationchannels.ts
@@ -14,13 +14,34 @@ router.get("/", (req, res) => {
   axios(opts)
     .then((response: any) => {
       // res.send(response.data);
-      res.render("notificationchannels", {
+      res.render("notificationchannels/channels", {
        data: response.data,
+       id2username: id2username.id2username,
+       jsonSyntaxHighlight: utils.jsonSyntaxHighlight,
+       format_date: utils.format_date,
+       });
+    })
+    .catch((error: Error) => utils.print_error(error));
+});
+
+router.get("/:channelid", (req, res) => {
+  let opts = request_opts;
+  let qs = req.query;
+  const channelid = req.params.channelid;
+  opts.url = `/notificationChannels/${channelid}`;
+  opts.params = qs;
+  axios(opts)
+    .then((response: any) => {
+      //res.send(response.data);
+      res.render("notificationchannels/channelid", {
+       data: response.data,
+       channelid: channelid,
        id2username: id2username.id2username,
        jsonSyntaxHighlight: utils.jsonSyntaxHighlight,
        });
     })
     .catch((error: Error) => utils.print_error(error));
 });
+
 
 module.exports = router;
